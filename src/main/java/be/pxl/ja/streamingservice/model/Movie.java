158853d94f85d2de7ba8959ccf59e2877731d074
Karel Sajdak <11902600@student.pxl.be>
2020-10-05 21:50:14 +0200

    Update Movie overeenkomstig met cursus

--- src/main/java/be/pxl/ja/streamingservice/model/Movie.java
+++ src/main/java/be/pxl/ja/streamingservice/model/Movie.java
@@ -1,62 +1,71 @@
 package be.pxl.ja.streamingservice.model;
-
 import java.time.LocalDate;
 
-public class Movie extends Content implements Playable{
-        public static final int LONG_PLAYING_TIME = 2 * 60 + 15;
+import static java.lang.Math.abs;
 
-        private String director;
-        private LocalDate releaseDate;
-        private int duration;
-        private Genre genre;
+public class Movie extends Content implements Playable{
+    public static final int LONG_PLAYING_TIME = 2 * 60 + 15;
+
+    private String director;
+    private LocalDate releaseDate;
+    private int duration;
+    private Genre genre;
 
     public Movie(String title, Rating maturityRating) {
         super(title, maturityRating);
     }
 
-        public String getDirector () {
-            return director;
-        }
-
-        public void setDirector(String director) {
-            this.director = director;
-        }
-
-        public LocalDate getReleaseDate () {
-            return releaseDate;
-        }
-
-        public void setReleaseDate(LocalDate releaseDate) {
-            this.releaseDate = releaseDate;
-        }
-
-        public int getDuration () {
-            return duration;
-        }
-
-        public void setDuration(int duration) {
-            this.duration = duration;
-        }
-
-        public boolean isLongPlayingTime() {
-            return duration >= LONG_PLAYING_TIME;
-        }
-
-        public void setGenre(Genre genre) {
-            this.genre = genre;
-        }
-
-        public Genre getGenre() {
-            return genre;
-        }
-        public void play() {
-            System.out.println("Playing " + this);
+    public String getDirector () {
+        return director;
     }
-        public void pause(){
-            System.out.println("Pausing " + this);
-        }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public LocalDate getReleaseDate () {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getDuration () {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = abs(duration);
+    }
+
+    public boolean isLongPlayingTime() {
+        return duration >= LONG_PLAYING_TIME;
+    }
+    public String getPlayingTime() {
+        // TODO: implement this method correctly
+        return "2 u 30 min";
+    }
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+    public void play() {
+        System.out.println("Playing " + this);
+    }
     @Override
-    public String toString () {
-            return String.format("%s (%tY)", super.getTitle(), releaseDate);
+    public void pause(){
+        System.out.println("Pausing " + this);
+    }
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder(super.toString());
+        if (releaseDate != null) {
+            builder.append(" (").append(releaseDate.getYear()).append(")");
+        }
+        return builder.toString();
     }
 }
