199819dbf7a255c3e7865da3d5054ad45f00ad2c
Karel Sajdak <11902600@student.pxl.be>
2020-10-20 10:24:20 +0200

    Added functionality to PasswordUtil. Added tests to PasswordUtilCalculateStrengthTest.

--- src/main/java/be/pxl/ja/streamingservice/util/PasswordUtil.java
+++ src/main/java/be/pxl/ja/streamingservice/util/PasswordUtil.java
@@ -3,8 +3,67 @@ package be.pxl.ja.streamingservice.util;
 public class PasswordUtil {
 
 	private static final String SPECIAL_CHARACTERS = "~!@#$%^&*()_-";
+	private static final String NUMBERS = "1234567890";
+	private static final String LETTERS = "azertyuiopqsdfghjklmwxcvbn";
 
 	public static int calculateStrength(String password) {
-		return 5;
+		int passwordStrength = 0;
+		if (password.length() >= 6 && password.length() < 10){
+			passwordStrength = 1;
+		}
+		else if (password.length() >= 10){
+			passwordStrength = 2;
+		}
+		if (passwordStrength > 0){
+			boolean breakloop = false;
+			for (char c : NUMBERS.toCharArray()){
+				for (char p : password.toCharArray()){
+					if (c == p){
+						passwordStrength += 2;
+						breakloop = true;
+					}
+				}
+				if (breakloop){
+					break;
+				}
+			}
+			for (char c : LETTERS.toCharArray()){
+				breakloop = false;
+				for (char p : password.toCharArray()){
+					if (c == p){
+						passwordStrength += 2;
+						breakloop = true;
+					}
+				}
+				if (breakloop){
+					break;
+				}
+			}
+			for (char c : LETTERS.toUpperCase().toCharArray()){
+				breakloop = false;
+				for (char p : password.toCharArray()){
+					if (c == p){
+						passwordStrength += 2;
+						breakloop = true;
+					}
+				}
+				if (breakloop){
+					break;
+				}
+			}
+			for (char c : SPECIAL_CHARACTERS.toCharArray()){
+				breakloop = false;
+				for (char p : password.toCharArray()){
+					if (c == p){
+						passwordStrength += 2;
+						breakloop = true;
+					}
+				}
+				if (breakloop){
+					break;
+				}
+			}
+		}
+		return passwordStrength;
 	}
 }
--- src/test/java/be/pxl/ja/streamingservice/util/PasswordUtilCalculateStrengthTest.java
+++ src/test/java/be/pxl/ja/streamingservice/util/PasswordUtilCalculateStrengthTest.java
@@ -1,5 +1,43 @@
 package be.pxl.ja.streamingservice.util;
 
-public class PasswordUtilCalculateStrengthTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
+public class PasswordUtilCalculateStrengthTest {
+    @Test
+    public void PasswordStrengthShouldBeZero(){
+        String password = "azert";
+
+        assertEquals(0, PasswordUtil.calculateStrength(password));
+    }
+    @Test void PasswordStrengthShouldBeThreeIfPasswordLengthIsSix(){
+        String password = "azerty";
+
+        assertEquals(3, PasswordUtil.calculateStrength(password));
+    }
+    @Test void PasswordStrengthShouldBeFourIfPasswordLengthIsTen(){
+        String password = "azertyuiop";
+
+        assertEquals(4, PasswordUtil.calculateStrength(password));
+    }
+    @Test void PasswordStrengthShouldBeThreeIfPasswordLengthIsSixAndPasswordIsNumbers(){
+        String password = "123456";
+
+        assertEquals(3, PasswordUtil.calculateStrength(password));
+    }
+    @Test void PasswordStrengthShouldBeThreeIfPasswordLengthIsSixAndPasswordIsUpperCase(){
+        String password = "AZERTY";
+
+        assertEquals(3, PasswordUtil.calculateStrength(password));
+    }
+    @Test void PasswordStrengthShouldBeThreeIfPasswordLengthIsSixAndPasswordIsSpecialCharacters(){
+        String password = "~!@#$%";
+
+        assertEquals(3, PasswordUtil.calculateStrength(password));
+    }
+    @Test void PasswordStrengthMaximumShouldBeTen(){
+        String password = "AZERTYUIOP1a$";
+
+        assertEquals(10, PasswordUtil.calculateStrength(password));
+    }
 }
